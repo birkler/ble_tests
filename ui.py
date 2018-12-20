51b751139aa25db62de4501ff0f8f4467c2dbdb9
jorgen@birkler.se <jorgen@birkler.se>
2018-12-19 18:26:41 -0800

    use python variable names to populate variables, labels and units in the UI.

--- ui.py
+++ ui.py
@@ -6,6 +6,7 @@ import time
 from bluepy import btle
 import time
 import binascii
+import re
 
 dev_name_uuid = btle.UUID(0x2A00)
 
@@ -17,34 +18,68 @@ genericServices = btle.UUID("8ab07720-1797-11e6-a7d5-0002a5d5c51b")
 
 device_id = "00:a0:50:ae:51:de"
 
-
-
-#for svc in dev.services:
-#    print(str(svc))
-
-
-
 class GlobalData:
 
     def __init__(self):
         self.devicelist = None
         self.connectedMacAddress = None
         self.is_running = True
-        self.HholdmV = 0
         self.i = 0
 
+        #0-2bit data
         self.X_00_Button = None
-        self.X_80_FirmwareVersion = 0
-        self.X_D0_macaddressH = None
-        self.X_D4_macaddressL = None
+        self.X_04_ChargingEnabled = None
+        self.X_08_AuxInd = None,
+        self.X_0C_AuxEnable = None,
+        self.X_10_StepperPowerCtrl = None
+
+        #1 byte data
+        self.X_48_ADC_VBus__mV = None
+        self.X_49_ADC_VMotor__mV = None
+        self.X_4A_ADC_V__mV = None
+        self.X_4B_ADC_VBus__mV = None
+        self.X_40_BoardRev__HEX = None
+        self.X_4C_Horiz_Run_Current__mA = None
+        self.X_4D_Vert_Run_Current__mA = None
+        self.X_50_Horiz_Hold__mA = None
+        self.X_51_Vert_Hold__mA = None
+        self.X_5C_Batt_Percent__Percent = None
+
+        #2byte data
+        self.X_80_FirmwareVersion__HEX = None
+        self.X_84_Axis_Limit_Min_VERT__tens_of_deg = None
+        self.X_88_Axis_Limit_Max_VERT__tens_of_deg = None
+        self.X_85_Axis_Limit_Min_HORIZ__tens_of_deg = None
+        self.X_89_Axis_Limit_Max_HORIZ__tens_of_deg = None
+        self.X_8c_Axis_Pos_HORIZ__tens_of_deg = None
+        self.X_8d_Axis_Pos_VERT__tens_of_deg = None
+        self.X_90_Run_Voltage__mV = None
+        self.X_94_Hold_Voltage__mV = None
+
+        #    0x98 : "Max Speed (in deg/s)",
+        #    0x9c : "Acceleration (in deg/s2)",
+        #    0xa0 : "Speed Limit Min. (in deg/s)",
+        #    0xa4 : "Speed Limit Max. (in deg/s)",
+        #    0xa8 : "Acceleration Limit Min. (in deg/s2)",
+        #    0xac : "Acceleration Limit Max. (in deg/s2)",
+        self.X_b0_Batt_Int_Resist__mOhm = None
+
+        #    0xcc : "Axis Position With ID (in 1/10 deg)",
+
+
+        #3byte data
+        self.X_C4_LED_RGB__HEX = None
+        self.X_D0_macaddressH__HEX = None
+        self.X_D4_macaddressL__HEX = None
+
 
         self.dictCmd = {}
 
 
-        for variable in vars(self):
-            if (variable[0:2]=='X_'):
-                cmdhex = int(variable[2:4],16)
-                self.dictCmd[cmdhex] = variable
+        for variable_name in vars(self):
+            if (variable_name[0:2]=='X_'):
+                cmdhex = int(variable_name[2:4],16)
+                self.dictCmd[cmdhex] = variable_name
         print vars(self)
 
 
@@ -93,7 +128,6 @@ class GlobalData:
             payload = (val_ch & 0xC0) >> 6
             cmd = (val_ch & 0xFC)
             chn = val_ch & 0x3
-            cmd_str = "UNKNOWN"
             val=0
             i+=1
             if (payload == 0) :
@@ -127,7 +161,7 @@ class BLERotorUpdateThread(threading.Thread):
         super(BLERotorUpdateThread, self).__init__(name=name)
         self.globalData = globalData
         self.macAddress = macAddress
-        self.daemon = True;
+        self.daemon = True
 
     def run(self):
         while (self.globalData.is_running):
@@ -150,7 +184,7 @@ class BLERotorUpdateThread(threading.Thread):
                 while (self.globalData.is_running):
                     ch_read = self.ch.read()
                     self.globalData.parseAndUpdate(ch_read)
-                    val = binascii.b2a_hex(ch_read)
+                    #val = binascii.b2a_hex(ch_read)
                     #print ("%s" % (val))
                     time.sleep(0.05)
                     self.globalData.i += 1
@@ -181,8 +215,8 @@ class RotorSelectForm(npyscreen.Form):
 
 class RotorHeroForm(npyscreen.Form):
     def __init__(self,parentApp, name,globalData):
-        super(RotorHeroForm, self).__init__(parentApp,name)
         self.globalData = globalData
+        super(RotorHeroForm, self).__init__(parentApp,name)
         print("!!@")
         self.updateForm()
 
@@ -194,22 +228,59 @@ class RotorHeroForm(npyscreen.Form):
         self.s_g  = self.add(npyscreen.TitleSlider, out_of=10, name = "Green")
         self.s_b  = self.add(npyscreen.TitleSlider, out_of=10, name = "Blue")
         self.button_rgb = self.add(npyscreen.Button,name="Set RGB")
-
-        self.rotorButton = self.add(npyscreen.TitleText, name = "Button:",value="")
-        self.horizHoldmV  = self.add(npyscreen.TitleText, name = "H Hold mV:",value=100)
-        self.firmwareVer  = self.add(npyscreen.TitleText, name = "Firmware Ver:",value=None)
         self.t  = self.add(npyscreen.TitleText, name = "BLE iter:",value="100")
-        self.fn = self.add(npyscreen.TitleFilename, name = "Filename:")
-        self.fn2 = self.add(npyscreen.TitleFilenameCombo, name="Filename2:")
-        self.dt = self.add(npyscreen.TitleDateCombo, name = "Date:")
+
+        
+        self.auto_widgets = {}
+        self.widget_unit = {}
+        cmds = self.globalData.dictCmd.keys();
+        cmds = sorted(cmds)
+        cnt = 0
+        for cmd in cmds:
+            variable_name = self.globalData.dictCmd[cmd]
+            var_name = "widget_" + variable_name
+            name_text_data = variable_name[5:]
+            unit_name = ""
+            label_and_unit = re.split("__",name_text_data)
+            name_text = label_and_unit[0]
+            if (len(label_and_unit) >= 2):
+                unit_name = label_and_unit[1]
+            self.auto_widgets[var_name] = variable_name
+            self.widget_unit[var_name] = unit_name
+            if (cnt & 0x1):
+                self.nextrelx= 50
+            else:
+                self.nextrelx= 0
+            widget = self.add(widgetClass = npyscreen.TitleText,max_width=45,begin_entry_at=23, name=name_text,value=0)
+            self.nextrely -= 1
+            if (cnt & 0x1):
+                self.nextrely += 1
+            setattr(self,var_name,widget)
+            cnt+=1
+
+
+        #self.batteryPercent  = self.add(npyscreen.TitleText, name = "Battery percent:",value=100)
+        #self.firmwareVer  = self.add(npyscreen.TitleText, name = "Firmware Ver:",value=None)
+        #self.fn = self.add(npyscreen.TitleFilename, name = "Filename:")
+        #self.fn2 = self.add(npyscreen.TitleFilenameCombo, name="Filename2:")
         self.i = 0
         
 
     def updateForm(self) :
-        self.rotorButton.value = "PRESSED" if (self.globalData.X_00_Button > 0) else "RELEASED"
-        self.horizHoldmV.value = self.globalData.HholdmV
+        for widgetname,variablename in self.auto_widgets.items():
+            variablevalue = getattr(self.globalData,variablename,0)
+            widget = getattr(self,widgetname)
+            unit = self.widget_unit[widgetname]
+            if (isinstance(variablevalue, int)):
+                if (unit == "HEX"):
+                    widget.value = "0x%08x %s" % (variablevalue,unit)
+                else:
+                    int_val = int(variablevalue)
+                    widget.value = "%d %s" % (int_val,unit)
+
+
         self.connected.value = self.globalData.connectedMacAddress
-        self.firmwareVer.value = "%d.%d.%d" % ((self.globalData.X_80_FirmwareVersion >> 8 & 0xF), (self.globalData.X_80_FirmwareVersion >> 4 & 0xF), (self.globalData.X_80_FirmwareVersion >> 0 & 0xF))
+        #self.firmwareVer.value = "%d.%d.%d" % ((self.globalData.X_80_FirmwareVersion >> 8 & 0xF), (self.globalData.X_80_FirmwareVersion >> 4 & 0xF), (self.globalData.X_80_FirmwareVersion >> 0 & 0xF))
         self.t.set_value("%d" % self.globalData.i)
 
 
